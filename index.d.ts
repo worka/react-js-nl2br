28eef18cc5b1e09da53965533c34d6b62fc92a73
Worka <blackworker@bk.ru>
2021-09-27 13:17:10 +1000

    init

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+import { ReactElement } from 'react';
+
+export function nl2br(str: string): string | ReactElement[];
+export function nl2br<T>(a: T): T;
---
+++ index.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react';
+
+export function nl2br(string, divider = <br/>) {
+    // if it is not a string
+    if (typeof string !== 'string') {
+        return string;
+    }
+
+    // if is no newline char in the string
+    if (!string.match('\n')) {
+        return string;
+    }
+
+    const fragments = string.split('\n');
+    const fragmentsCount = fragments.length;
+
+    return fragments.map((line, i) => {
+        // in the last fragment not display <br>
+        const breakPoint = i + 1 < fragmentsCount ? divider : false;
+
+        return <Fragment key={ i }>{ line }{ breakPoint }</Fragment>;
+    });
+}
